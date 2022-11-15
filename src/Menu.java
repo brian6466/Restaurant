f23542690b0c6f1bae398dfc936ab71469833e6e
BrownBadger221 <117844859+BrownBadger221@users.noreply.github.com>
2022-11-15 15:26:36 +0000

    Add files via upload

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,37 @@
-
 public class Menu {
-
 }
+class Starters extends Menu{
+    String a1 = "Poppadoms" ;
+    String b1 = "Vegetable SpringRolls" ;
+    String c1 = "Chicken Skewers" ;
+    String d1 = "Garlic Bread" ;
+    String e1 = "Soup of the day" ;
+    String[] soup = {"Tomato", "Carrot", "Pumpkin", "Lentil", "Mushroom"}; // array of soups
+}
+
+
+class Main_Course extends Menu{
+    String a2 = "Chicken Parmesan";
+    String b2 = "Grilled Steak";
+    String c2 = "Cilantro Fish";
+    String d2 = "Prawn Pasta";
+    String e2 = "Vegetarian Casserole";
+}
+
+class Deserts extends Menu {
+    String a3 = "Ice Cream";
+    String b3 = "Cheese cake";
+    String c3 = "Apple Pie";
+    String d3 = "Brownies";
+    String e3 = "Cookies";
+}
+
+class Drinks extends Menu{
+    String a4 = "Fanta";
+    String b4 = "Coke";
+    String c4 = "Diet Coke";
+    String d4 = "7 Up";
+    String e4 = "Juices";
+}
+
+
