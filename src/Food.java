1f86e5403964d691d4b6468588503be8800c67b3
Brian <99266888+brian6466@users.noreply.github.com>
2022-11-17 14:51:44 +0000

    Add files via upload

--- src/Food.java
+++ src/Food.java
@@ -1,10 +1,7 @@
-import java.util.ArrayList;
+
 
 public class Food {
-	ArrayList<Food> starters = new ArrayList<Food>();
-	ArrayList<Food> maincourse = new ArrayList<Food>();
-	ArrayList<Food> desert = new ArrayList<Food>();
-	ArrayList<Food> drinks = new ArrayList<Food>();
+	
 	private String foodName;
 	private double foodPrice;
 	
@@ -13,37 +10,7 @@ public class Food {
 		this.setFoodPrice(foodPrice);
 	}
 	
-	public void addStarter(Food f) {
-		starters.add(f);
-	}
-	
-	public void removeStarter(Food f) {
-		starters.remove(f);
-	}
-	
-	public void addMaincourse(Food f) {
-		maincourse.add(f);
-	}
-	
-	public void removeMaincourse(Food f) {
-		maincourse.remove(f);
-	}
-	
-	public void addDesert(Food f) {
-		desert.add(f);
-	}
-	
-	public void removeDesert(Food f) {
-		desert.remove(f);
-	}
-	
-	public void addDrinks(Food f) {
-		drinks.add(f);
-	}
-	
-	public void removeDrinks(Food f) {
-		drinks.remove(f);
-	}
+
 
 	public String getFoodName() {
 		return foodName;
