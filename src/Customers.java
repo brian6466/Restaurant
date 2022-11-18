217d5f13d43323dfbb8308b5d37c4585905ba9ea
Brian <99266888+brian6466@users.noreply.github.com>
2022-11-18 17:20:03 +0000

    Add files via upload

--- src/Customers.java
+++ src/Customers.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 
 public class Customers extends Person {
 
 	private int customerID = 0;
+	ArrayList<Food> selectedOrder = new ArrayList<Food>();
 	
 	public Customers(String email, String number, String name) {
 		super(email, number, name);
@@ -14,10 +16,17 @@ public class Customers extends Person {
 	public void setCustomerID(int customerID) {
 		this.customerID = customerID;
 	}
+	
 	public void PayBill(){
-		Sytem.out.println("Bill has been paid ");
+		System.out.println("Bill has been paid ");
 	}
-
 	
+	public void addToOrder(Food f) {
+		selectedOrder.add(f);
+	}
 	
+	public ArrayList<Food> getOrder(){
+		return selectedOrder;
+		
+	}
 }
--- src/Waiter.java
+++ src/Waiter.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
 
 public class Waiter extends Staff{
+	
 
-	public Waiter(String email, String number, String name) {
-		super(email, number, name);
+	public Waiter(String email, String number, String name, int salary) {
+		super(email, number, name, salary);
+	}
+	
+	
+	public void completeOrder(Customers C) {
+		//Not implemented yet.
+		double totalprice = 0;
+		
+		for (int i = 0; i <= C.getOrder().size(); i++) {
+			totalprice = totalprice + C.getOrder().get(i).getFoodPrice();
+		}
 	}
 
 }
