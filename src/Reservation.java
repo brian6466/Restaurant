1d764983925e456525d07d2d3d66f99222989b57
D4RA <101725672+D4RA@users.noreply.github.com>
2022-11-23 23:38:43 +0000

    Add files via upload

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,27 +1,44 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class Reservation{
+public class Reservation {
     private int ReservationId = 0;
-    private String ReservationName ="";
-    private int ReservationSeatNo =0;
+    private String ReservationName = "";
+    private int ReservationSeatNo = 0;
     private int ReservationPhoneNo = 0;
-    
-    public Reservation( String ReservationName,int ReservationSeatNo,int ReservationPhoneNo){
-       ReservationId++;
+
+    public Reservation(String ReservationName, int ReservationSeatNo, int ReservationPhoneNo) {
+        ReservationId++;
         this.ReservationName = ReservationName;
         this.ReservationSeatNo = ReservationSeatNo;
         this.ReservationPhoneNo = ReservationPhoneNo;
-
 
     }
 
     ArrayList<Reservation> reservations = new ArrayList<Reservation>();
 
-    public void AddReservation(Reservation r){
+    public void AddReservation(Reservation r) {
         reservations.add(r);
     }
 
-    public void CancelReservation(Reservation r){
-         reservations.remove(r);
+    public void CancelReservation(Reservation r) {
+        reservations.remove(r);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+        File csvFile = new File("reservation.csv");
+        PrintWriter out = new PrintWriter(csvFile);
+
+        for (Reservation reservation : reservations) {
+            out.println(reservations);
+
+        }
+        out.close();
+
     }
 }
