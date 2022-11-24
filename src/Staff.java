1562d75cf855b4fd768facfce119903730f9f362
BrownBadger221 <117844859+BrownBadger221@users.noreply.github.com>
2022-11-24 07:02:48 +0000

    Update Staff.java

--- src/Staff.java
+++ src/Staff.java
@@ -1,19 +1,33 @@
 
 public class Staff extends Person {
 
-	private int salary;
+    private int salary;
 
-	public Staff(String number, String name, int salary) {
-		super(number, name);
-		this.setSalary(salary);
-	}
+    /**
+     * constructor
+     * @param number
+     * @param name
+     * @param salary
+     */
+    public Staff(String number, String name, int salary) {
+        super(number, name);
+        this.setSalary(salary);
+    }
 
-	public int getSalary() {
-		return salary;
-	}
+    /**
+     * gets salary
+     * @return salary
+     */
+    public int getSalary() {
+        return salary;
+    }
 
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
+    /**
+     * sets salary
+     * @param salary
+     */
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 
 }
