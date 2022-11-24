eda1e4808c659aea990a0158fd277be507be1c2d
BrownBadger221 <117844859+BrownBadger221@users.noreply.github.com>
2022-11-24 07:04:37 +0000

    Update Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -4,82 +4,123 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class Reservation {
-	private int ReservationId = 0;
-	private String ReservationName = "";
-	private int ReservationSeatNo = 0;
-	private int ReservationPhoneNo = 0;
-	private String time;
-	private int capacity;
+    private int ReservationId = 0;
+    private String ReservationName = "";
+    private int ReservationSeatNo = 0;
+    private int ReservationPhoneNo = 0;
+    private String time;
+    private int capacity;
 
-	public Reservation(Restaurant r, String ReservationName, int ReservationSeatNo, int capacity,
-			int ReservationPhoneNo, String time) {
-		setReservationId(getReservationId() + 1);
-		this.ReservationName = ReservationName;
-		this.ReservationSeatNo = ReservationSeatNo;
-		this.ReservationPhoneNo = ReservationPhoneNo;
-		this.time = time;
-		this.capacity = capacity;
-		Reservation res = new Reservation(ReservationName, ReservationSeatNo, capacity, ReservationPhoneNo, time);
-		r.reservations.add(res);
-		for (int i = 0; i < r.getTables().size(); i++) {
-			if (r.getTables().get(i).getTableNo() == ReservationSeatNo) {
-				r.unavailableTables.add(r.tables.get(i));
-				r.tables.remove(i);
-			}
-		}
-	}
+    /**
+     *
+     * @param r
+     * @param ReservationName
+     * @param ReservationSeatNo
+     * @param capacity
+     * @param ReservationPhoneNo
+     * @param time
+     */
+    public Reservation(Restaurant r, String ReservationName, int ReservationSeatNo, int capacity,
+                       int ReservationPhoneNo, String time) {
+        setReservationId(getReservationId() + 1);
+        this.ReservationName = ReservationName;
+        this.ReservationSeatNo = ReservationSeatNo;
+        this.ReservationPhoneNo = ReservationPhoneNo;
+        this.time = time;
+        this.capacity = capacity;
+        Reservation res = new Reservation(ReservationName, ReservationSeatNo, capacity, ReservationPhoneNo, time);
+        r.reservations.add(res);
+        for (int i = 0; i < r.getTables().size(); i++) {
+            if (r.getTables().get(i).getTableNo() == ReservationSeatNo) {
+                r.unavailableTables.add(r.tables.get(i));
+                r.tables.remove(i);
+            }
+        }
+    }
 
-	public Reservation(String ReservationName, int ReservationSeatNo, int capacity, int ReservationPhoneNo,
-			String time) {
-		setReservationId(getReservationId() + 1);
-		this.ReservationName = ReservationName;
-		this.ReservationSeatNo = ReservationSeatNo;
-		this.ReservationPhoneNo = ReservationPhoneNo;
-		this.time = time;
-		this.capacity = capacity;
+    /**
+     *
+     * @param ReservationName
+     * @param ReservationSeatNo
+     * @param capacity
+     * @param ReservationPhoneNo
+     * @param time
+     */
+    public Reservation(String ReservationName, int ReservationSeatNo, int capacity, int ReservationPhoneNo,
+                       String time) {
+        setReservationId(getReservationId() + 1);
+        this.ReservationName = ReservationName;
+        this.ReservationSeatNo = ReservationSeatNo;
+        this.ReservationPhoneNo = ReservationPhoneNo;
+        this.time = time;
+        this.capacity = capacity;
 
-	}
+    }
 
-	public String toString() {
-		String result = "";
-		result += "ReservationName: " + ReservationName + "\n";
-		result += "ReservationSeatNo: " + ReservationSeatNo + "\n";
-		result += "ReservationPhoneNo: " + ReservationPhoneNo + "\n";
-		result += "time: " + time + "\n";
-		result += "capacity: " + capacity + "\n";
-		return result;
-	}
+    /**
+     * toString method
+     * @return resulted string
+     */
+    public String toString() {
+        String result = "";
+        result += "ReservationName: " + ReservationName + "\n";
+        result += "ReservationSeatNo: " + ReservationSeatNo + "\n";
+        result += "ReservationPhoneNo: " + ReservationPhoneNo + "\n";
+        result += "time: " + time + "\n";
+        result += "capacity: " + capacity + "\n";
+        return result;
+    }
 
-	public static void main(String[] args) throws FileNotFoundException {
-		ArrayList<Reservation> reservations = new ArrayList<Reservation>();
-		File csvFile = new File("reservation.csv");
-		PrintWriter out = new PrintWriter(csvFile);
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+        File csvFile = new File("reservation.csv");
+        PrintWriter out = new PrintWriter(csvFile);
 
-		for (Reservation reservation : reservations) {
-			out.println(reservation.toString());
+        for (Reservation reservation : reservations) {
+            out.println(reservation.toString());
 
-		}
-		out.close();
+        }
+        out.close();
 
-	}
+    }
 
-	public int getReservationId() {
-		return ReservationId;
-	}
+    /**
+     * gets reservation id
+     * @return reservationId
+     */
+    public int getReservationId() {
+        return ReservationId;
+    }
 
-	public int getReservationPhoneNo() {
-		return ReservationPhoneNo;
-	}
+    /**
+     * gets reservation phone number
+     * @return reservationPhoneNo
+     */
+    public int getReservationPhoneNo() {
+        return ReservationPhoneNo;
+    }
 
-	public int getReservationSeatNo() {
-		return ReservationSeatNo;
-	}
+    /**
+     * gets reservation seat number
+     * @return reservationSeatNo
+     */
+    public int getReservationSeatNo() {
+        return ReservationSeatNo;
+    }
 
-	public int getCapacity() {
-		return capacity;
-	}
+    /**
+     * gets capacity of restaurant
+     * @return capacity
+     */
+    public int getCapacity() {
+        return capacity;
+    }
 
-	public void setReservationId(int reservationId) {
-		ReservationId = reservationId;
-	}
+    /**
+     * sets reservation id
+     * @param  reservationId
+     */
+    public void setReservationId(int reservationId) {
+        ReservationId = reservationId;
+    }
 }
