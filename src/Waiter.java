397fc48ad72741d4488d60e346f50ffd52f1137b
BrownBadger221 <117844859+BrownBadger221@users.noreply.github.com>
2022-11-24 07:03:38 +0000

    Update Waiter.java

--- src/Waiter.java
+++ src/Waiter.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
 
 public class Waiter extends Staff {
+    /**
+     * constructor
+     * @param number
+     * @param name
+     * @param salary
+     */
+    public Waiter(String number, String name, int salary) {
+        super(number, name, salary);
+    }
 
-	public Waiter(String number, String name, int salary) {
-		super(number, name, salary);
-	}
+    /**
+     * complete order method
+     * @param C
+     */
+    public void completeOrder(Customers C) {
+        double totalprice = 0;
 
-	public void completeOrder(Customers C) {
-		double totalprice = 0;
-
-		for (int i = 0; i <= C.getOrder().size(); i++) {
-			totalprice = totalprice + C.getOrder().get(i).getFoodPrice();
-		}
-	}
+        for (int i = 0; i <= C.getOrder().size(); i++) {
+            totalprice = totalprice + C.getOrder().get(i).getFoodPrice();
+        }
+    }
 
 }
