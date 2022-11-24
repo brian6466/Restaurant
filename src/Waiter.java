1c6daee1d388f98b9b0f951b393491490e459bbd
Brian <99266888+brian6466@users.noreply.github.com>
2022-11-24 08:20:30 +0000

    Small fix

--- src/Waiter.java
+++ src/Waiter.java
@@ -15,12 +15,13 @@ public class Waiter extends Staff {
      * complete order method
      * @param C
      */
-    public void completeOrder(Customers C) {
+  public double completeOrder(Customers C) {
         double totalprice = 0;
 
         for (int i = 0; i < C.getOrder().size(); i++) {
             totalprice = totalprice + C.getOrder().get(i).getFoodPrice();
         }
+        return totalprice;
     }
 
 }
