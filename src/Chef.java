03e8d44052ff5987a164a2aca835155619fd863a
BrownBadger221 <117844859+BrownBadger221@users.noreply.github.com>
2022-11-24 06:31:43 +0000

    Update Chef.java

--- src/Chef.java
+++ src/Chef.java
@@ -1,10 +1,24 @@
 
 public class Chef extends Staff {
 
-	public Chef(String number, String name, int salary) {
-		super(number, name, salary);
-	}
+    public Chef(String number, String name, int salary) {
+        super(number, name, salary);
+    }
 
-	// Method to say if order is ready
+    // Method to say if order is ready
+    public void CheckOrder(boolean order){
+        if(order == true){
+            System.out.println("Your order is complete");
+        }
+        else if (order == false){
+            System.out.println("Your order has not been completed, Please wait patiently");
+            System.out.println("Thank You :)");
+        }
+    }
+
+    public static void main(String[] args) {
+        Chef c = new Chef("1","joe",1000);
+        c.CheckOrder(true);
+    }
 
 }
