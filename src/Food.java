bc748867bc7acab857c74c226dac1f60161571af
BrownBadger221 <117844859+BrownBadger221@users.noreply.github.com>
2022-11-24 07:05:09 +0000

    Update Food.java

--- src/Food.java
+++ src/Food.java
@@ -1,34 +1,58 @@
 
 public class Food {
 
-	private String foodName;
-	private double foodPrice;
+    private String foodName;
+    private double foodPrice;
 
-	public Food(String foodName, double foodPrice) {
-		this.setFoodName(foodName);
-		this.setFoodPrice(foodPrice);
-	}
+    /**
+     * constructor
+     * @param foodName
+     * @param foodPrice
+     */
+    public Food(String foodName, double foodPrice) {
+        this.setFoodName(foodName);
+        this.setFoodPrice(foodPrice);
+    }
 
-	public String getFoodName() {
-		return foodName;
-	}
+    /**
+     * gets food name
+     * @return foodName
+     */
+    public String getFoodName() {
+        return foodName;
+    }
 
-	public void setFoodName(String foodName) {
-		this.foodName = foodName;
-	}
+    /**
+     * sets food name
+     * @param foodName
+     */
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
 
-	public double getFoodPrice() {
-		return foodPrice;
-	}
+    /**
+     * gets food price
+     * @return foodPrice
+     */
+    public double getFoodPrice() {
+        return foodPrice;
+    }
+    /**
+     * sets food price
+     * @param foodPrice
+     */
+    public void setFoodPrice(double foodPrice) {
+        this.foodPrice = foodPrice;
+    }
 
-	public void setFoodPrice(double foodPrice) {
-		this.foodPrice = foodPrice;
-	}
-
-	public String toString() {
-		String result = "";
-		result += "Food Name: " + foodName + "\n";
-		result += "Food Price: " + foodPrice + "\n";
-		return result;
-	}
+    /**
+     * toString method
+     * @return resulted string
+     */
+    public String toString() {
+        String result = "";
+        result += "Food Name: " + foodName + "\n";
+        result += "Food Price: " + foodPrice + "\n";
+        return result;
+    }
 }
