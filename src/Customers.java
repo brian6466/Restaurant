948b46416a3db4b6a349fcb8f33537aceeb89cd9
Brian <99266888+brian6466@users.noreply.github.com>
2022-11-24 07:17:46 +0000

    Added docs

--- src/Customers.java
+++ src/Customers.java
@@ -4,28 +4,50 @@ public class Customers extends Person {
 
 	private int customerID = 0;
 	ArrayList<Food> selectedOrder = new ArrayList<Food>();
-
+	
+	/**
+    Customer contructor.
+    @param number
+    @param name
+	 */
 	public Customers(String number, String name) {
 		super(number, name);
 		setCustomerID(getCustomerID() + 1);
 	}
 
+	/**
+    Gets customer ID
+    @return customerID
+	 */
 	public int getCustomerID() {
 		return customerID;
 	}
 
+	/**
+    Sets customer ID
+    @param customerID
+	 */
 	public void setCustomerID(int customerID) {
 		this.customerID = customerID;
 	}
 
+	//Pays the Bill
 	public void PayBill() {
 		System.out.println("Bill has been paid ");
 	}
 
+	/**
+    Adds food to order arraylist
+    @param Food
+	 */
 	public void addToOrder(Food f) {
 		selectedOrder.add(f);
 	}
 
+	/**
+    returns order array list
+    @return ArrayList
+	 */
 	public ArrayList<Food> getOrder() {
 		return selectedOrder;
 
