e35c9a2ca877d1ac42fc3ab89833a54e0d157a86
BrownBadger221 <117844859+BrownBadger221@users.noreply.github.com>
2022-11-24 09:13:05 +0000

    Update Bill.java

--- src/Bill.java
+++ src/Bill.java
@@ -9,32 +9,32 @@ public class Bill {
 
 
 
-        private LocalDateTime date; //represents current date and time
+    private LocalDateTime date; //represents current date and time
 
-        private double tip;
-        private double amount;
-        private double balance;
-        private String description;
-        private File filesOrders = new File("billfororder.csv");
-        private PrintWriter out = new PrintWriter(filesOrders);
+    private double tip;
+    private double amount;
+    private double balance;
+    private String description;
+    private File filesOrders = new File("billfororder.csv");
+    private PrintWriter out = new PrintWriter(filesOrders);
 
-        /**
+    /**
 
-        @param date
-        @param tip
-        @param amount
-        @param balance
-        @param description
+     @param date (Local date time)
+     @param tip (double tip received)
+     @param amount (double amount of bill)
+     @param balance (double balance in bill)
+     @param description (String Description of bill)
 
-         */
-        public Bill(LocalDateTime date, double tip, double amount, double balance, String description) throws FileNotFoundException {
-            this.date = LocalDateTime.now();
-            this.tip = tip;
-            this.amount = amount;
-            this.balance = balance;
-            this.description = description;
-        }
-        //Initializing objects of class bill but leaves out tip as it doesn't always occur and throwing except for file not found
+     */
+    public Bill(LocalDateTime date, double tip, double amount, double balance, String description) throws FileNotFoundException {
+        this.date = LocalDateTime.now();
+        this.tip = tip;
+        this.amount = amount;
+        this.balance = balance;
+        this.description = description;
+    }
+    //Initializing objects of class bill but leaves out tip as it doesn't always occur and throwing except for file not found
     public Bill(LocalDateTime date,double amount,double balance,String description) throws FileNotFoundException {
         this.date = LocalDateTime.now();
         this.amount = amount;
@@ -46,20 +46,20 @@ public class Bill {
      * toString method
      * @return resulted string
      */
-        public String toString(){
-            String result = "";
-            result += "Date: " + date + "\n";
-            result += "Tip: " + tip +"\n";
-            result += "Amount: " + amount + "\n";
-            result += "Balance: " + balance + "\n";
-            result += "Description: " + description + "\n";
-            return result;
-        }
+    public String toString(){
+        String result = "";
+        result += "Date: " + date + "\n";
+        result += "Tip: " + tip +"\n";
+        result += "Amount: " + amount + "\n";
+        result += "Balance: " + balance + "\n";
+        result += "Description: " + description + "\n";
+        return result;
+    }
 
     public static void main(String[] args) throws FileNotFoundException{
 
-            Bill Order1 = new Bill(LocalDateTime.now(),10,100,150,"MainCourse");
-            Bill Order2 = new Bill(LocalDateTime.now(),35,450,"Desserts");
+        Bill Order1 = new Bill(LocalDateTime.now(),10,100,150,"MainCourse");
+        Bill Order2 = new Bill(LocalDateTime.now(),35,450,"Desserts");
         ArrayList<Bill>billorder = new ArrayList<Bill>(); //creates arraylist of bill
         File csvFile = new File("billfororder.csv");//creates cvsfile object
         PrintWriter outer = new PrintWriter(csvFile);
@@ -68,15 +68,16 @@ public class Bill {
         billorder.add(Order2);
 
         /**
-         * creates csv
+         * creates csv file
          * prints data to csv
          */
         for
         (Bill b: billorder)
         {
             outer.println(b.toString());
-        outer.close(); 
+            outer.close();
+
+        }
 
     }
-
-}
+        
