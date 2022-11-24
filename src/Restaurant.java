a0d2e46b0447718c028fb955e9f2f96964b500f2
BrownBadger221 <117844859+BrownBadger221@users.noreply.github.com>
2022-11-24 07:02:01 +0000

    Update Restaurant.java

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,96 +1,170 @@
 import java.util.ArrayList;
 
+
 public class Restaurant {
-	ArrayList<Table> tables = new ArrayList<Table>();
-	ArrayList<Table> unavailableTables = new ArrayList<Table>();
-	ArrayList<Food> starters = new ArrayList<Food>();
-	ArrayList<Food> maincourse = new ArrayList<Food>();
-	ArrayList<Food> desert = new ArrayList<Food>();
-	ArrayList<Food> drinks = new ArrayList<Food>();
-	ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+    ArrayList<Table> tables = new ArrayList<Table>();
+    ArrayList<Table> unavailableTables = new ArrayList<Table>();
+    ArrayList<Food> starters = new ArrayList<Food>();
+    ArrayList<Food> maincourse = new ArrayList<Food>();
+    ArrayList<Food> desert = new ArrayList<Food>();
+    ArrayList<Food> drinks = new ArrayList<Food>();
+    ArrayList<Reservation> reservations = new ArrayList<Reservation>();
 
-	private int restaurantID;
+    private int restaurantID;
+    
+    /**
+     * constructor
+     * @param restaurantID
+     * */
+    public Restaurant(int restaurantID) {
+        this.restaurantID = restaurantID;
+    }
+    
+    /**
+     * @return reservations
+     * */
+    public ArrayList<Reservation> getReservations() {
+        return reservations;
+    }
 
-	public Restaurant(int restaurantID) {
-		this.restaurantID = restaurantID;
-	}
+    /**
+     * 
+     * @return starters
+     */
 
-	public ArrayList<Reservation> getReservations() {
-		return reservations;
-	}
+    public ArrayList<Food> getStarters() {
+        return starters;
+    }
 
-	public ArrayList<Food> getStarters() {
-		return starters;
-	}
+    /**
+     * method to remove reservations
+     * @param r
+     */
 
-	public void removeReservation(Reservation r) {
-		for (int i = 0; i < reservations.size(); i++) {
-			if (reservations.get(i).getReservationSeatNo() == r.getReservationSeatNo()) {
-				Table t = new Table(r.getReservationSeatNo(), r.getCapacity());
-				tables.add(t);
-				unavailableTables.remove(t);
-			}
-		}
-		reservations.remove(r);
-	}
+    public void removeReservation(Reservation r) {
+        for (int i = 0; i < reservations.size(); i++) {
+            if (reservations.get(i).getReservationSeatNo() == r.getReservationSeatNo()) {
+                Table t = new Table(r.getReservationSeatNo(), r.getCapacity());
+                tables.add(t);
+                unavailableTables.remove(t);
+            }
+        }
+        reservations.remove(r);
+    }
 
-	public int getRestaurantID() {
-		return restaurantID;
-	}
+    /**
+     * gets restaurant ID
+     * @return restaurantID
+     */
+    public int getRestaurantID() {
+        return restaurantID;
+    }
 
-	public void setRestaurantID(int restaurantID) {
-		this.restaurantID = restaurantID;
-	}
+    /**
+     * sets restaurant ID
+     * @param restaurantID
+     */
+    public void setRestaurantID(int restaurantID) {
+        this.restaurantID = restaurantID;
+    }
 
-	public void addStarter(Food f) {
-		starters.add(f);
-	}
+    /**
+     * add starters to food
+     * @param f
+     */
+    public void addStarter(Food f) {
+        starters.add(f);
+    }
 
-	public void removeStarter(Food f) {
-		starters.remove(f);
-	}
+    /**
+     * remove starter from food
+     * @param f
+     */
+    public void removeStarter(Food f) {
+        starters.remove(f);
+    }
 
-	public void addMaincourse(Food f) {
-		maincourse.add(f);
-	}
+    /**
+     * adds main course to food
+     * @param f
+     */
+    public void addMaincourse(Food f) {
+        maincourse.add(f);
+    }
 
-	public void removeMaincourse(Food f) {
-		maincourse.remove(f);
-	}
+    /**
+     * remove main course from food
+     * @param f
+     */
+    public void removeMaincourse(Food f) {
+        maincourse.remove(f);
+    }
 
-	public void addDesert(Food f) {
-		desert.add(f);
-	}
+    /**
+     * adds desserts to food
+     * @param f
+     */
+    public void addDesert(Food f) {
+        desert.add(f);
+    }
 
-	public void removeDesert(Food f) {
-		desert.remove(f);
-	}
+    /**
+     * remove desserts from food
+     * @param f
+     */
+    public void removeDesert(Food f) {
+        desert.remove(f);
+    }
 
-	public void addDrinks(Food f) {
-		drinks.add(f);
-	}
+    /**
+     * adds drinks to food
+     * @param f
+     */
+    public void addDrinks(Food f) {
+        drinks.add(f);
+    }
 
-	public void removeDrinks(Food f) {
-		drinks.remove(f);
-	}
+    /**
+     * remove drinks from food
+     * @param f
+     */
+    public void removeDrinks(Food f) {
+        drinks.remove(f);
+    }
 
-	public void addTable(Table t) {
-		tables.add(t);
-	}
+    /**
+     * adds table
+     * @param t
+     */
+    public void addTable(Table t) {
+        tables.add(t);
+    }
 
-	public void removeTable(Table t) {
-		tables.remove(t);
-		unavailableTables.add(t);
+    /**
+     * removes table
+     * @param t
+     */
+    public void removeTable(Table t) {
+        tables.remove(t);
+        unavailableTables.add(t);
 
-	}
+    }
 
-	public void addBackTable(Table t) {
-		unavailableTables.remove(t);
-		tables.add(t);
-	}
+    /**
+     * adds used table
+     * @param t
+     */
+    public void addBackTable(Table t) {
+        unavailableTables.remove(t);
+        tables.add(t);
+    }
 
-	public ArrayList<Table> getTables() {
-		return tables;
-	}
+    /**
+     * method to get tables
+     * @return tables
+     */
+    public ArrayList<Table> getTables() {
+        return tables;
+    }
 
 }
