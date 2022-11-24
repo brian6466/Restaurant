ac0cace9be859ee6da270106058f1b0ee4c80b1f
Brian <99266888+brian6466@users.noreply.github.com>
2022-11-24 06:28:24 +0000

    added some java docs

--- src/Table.java
+++ src/Table.java
@@ -4,27 +4,52 @@ public class Table {
 	private int capacity;
 	private int tableNo;
 
+	 /**
+     * Makes a Table Object
+     * @param Table Number
+     * @param Capacity
+     */
 	public Table(int tableNo, int capacity) {
 		this.capacity = capacity;
 		this.tableNo = tableNo;
 	}
 
+	 /**
+     * Gets Capacity
+     * @return Integer
+     */
 	public int getCapacity() {
 		return capacity;
 	}
 
+	/**
+     * Sets Capacity
+     * @param capacity
+     */
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
 
+	/**
+     * Gets Table Number
+     * @return Integer
+     */
 	public int getTableNo() {
 		return tableNo;
 	}
 
+	/**
+     * Sets Table Number
+     * @param Table Number
+     */
 	public void setTableNo(int tableNo) {
 		this.tableNo = tableNo;
 	}
 
+	/**
+     * Prints Book Object
+     * @return String
+     */
 	public String toString() {
 		String result = "";
 		result += "Capacity: " + capacity;
