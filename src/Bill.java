f9775263adfaa5a91cf0528f911688935ddf6ce4
Drickido77 <117481056+Drickido77@users.noreply.github.com>
2022-11-24 07:05:34 +0000

    Update Bill.java

--- src/Bill.java
+++ src/Bill.java
@@ -7,57 +7,62 @@ import java.util.ArrayList;
 
 public class Bill {
 
-	private LocalDateTime date; // represents current date and time
 
-	private double tip;
-	private double amount;
-	private double balance;
-	private String description;
-	private File filesOrders = new File("billfororder.csv");
-	private PrintWriter out = new PrintWriter(filesOrders);
 
-	public Bill(LocalDateTime date, double tip, double amount, double balance, String description)
-			throws FileNotFoundException {
-		this.date = LocalDateTime.now();
-		this.tip = tip;
-		this.amount = amount;
-		this.balance = balance;
-		this.description = description;
-	}
+        private LocalDateTime date; //represents current date and time
 
-	public Bill(LocalDateTime date, double amount, double balance, String description) throws FileNotFoundException {
-		this.date = LocalDateTime.now();
-		this.amount = amount;
-		this.balance = balance;
-		this.description = description;
-	}
+        private double tip;
+        private double amount;
+        private double balance;
+        private String description;
+        private File filesOrders = new File("billfororder.csv");
+        private PrintWriter out = new PrintWriter(filesOrders);
 
-	public String toString() {
-		String result = "";
-		result += "Date: " + date + "\n";
-		result += "Tip: " + tip + "\n";
-		result += "Amount: " + amount + "\n";
-		result += "Balance: " + balance + "\n";
-		result += "Description: " + description + "\n";
-		return result;
-	}
+        //Initializing objects of class bill and throwing except for file not found
+        public Bill(LocalDateTime date, double tip, double amount, double balance, String description) throws FileNotFoundException {
+            this.date = LocalDateTime.now();
+            this.tip = tip;
+            this.amount = amount;
+            this.balance = balance;
+            this.description = description;
+        }
+        ////Initializing objects of class bill but leaves out tip as it doesn't always occur and throwing except for file not found
+    public Bill(LocalDateTime date,double amount,double balance,String description) throws FileNotFoundException {
+        this.date = LocalDateTime.now();
+        this.amount = amount;
+        this.balance = balance;
+        this.description = description;
+    }
 
-	public static void main(String[] args) throws FileNotFoundException {
 
-		Bill Order1 = new Bill(LocalDateTime.now(), 10, 100, 150, "MainCourse");
-		Bill Order2 = new Bill(LocalDateTime.now(), 35, 450, "Desserts");
-		ArrayList<Bill> billorder = new ArrayList<Bill>();
-		File csvFile = new File("billfororder.csv");
-		PrintWriter outer = new PrintWriter(csvFile);
+        public String toString(){
+            String result = "";
+            result += "Date: " + date + "\n";
+            result += "Tip: " + tip +"\n";
+            result += "Amount: " + amount + "\n";
+            result += "Balance: " + balance + "\n";
+            result += "Description: " + description + "\n";
+            return result;
+        }
 
-		billorder.add(Order1);
-		billorder.add(Order2);
+    public static void main(String[] args) throws FileNotFoundException{
 
-		for (Bill b : billorder) {
-			outer.println(b.toString());
-		}
-		outer.close();
+            Bill Order1 = new Bill(LocalDateTime.now(),10,100,150,"MainCourse");
+            Bill Order2 = new Bill(LocalDateTime.now(),35,450,"Desserts");
+        ArrayList<Bill>billorder = new ArrayList<Bill>(); //creates arraylist of bill
+        File csvFile = new File("billfororder.csv");//creates cvsfile object
+        PrintWriter outer = new PrintWriter(csvFile);
 
-	}
+        billorder.add(Order1);
+        billorder.add(Order2);
+
+        for
+        (Bill b: billorder)
+        {
+            outer.println(b.toString());//creates cvsfile
+        }
+        outer.close(); //prints the data into the csv
+
+    }
 
 }
