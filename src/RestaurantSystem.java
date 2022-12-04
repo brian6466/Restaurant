d16607d52886c572765e924f9cd0e7577f7888b0
Brian <99266888+brian6466@users.noreply.github.com>
2022-12-04 18:33:42 +0000

    Update RestaurantSystem.java

--- src/RestaurantSystem.java
+++ src/RestaurantSystem.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
 
-/**
- * A system to manage appointments.
- */
 public class RestaurantSystem {
 	public static void main(String[] args) throws IOException {
 		RestaurantUI menu = new RestaurantUI();
